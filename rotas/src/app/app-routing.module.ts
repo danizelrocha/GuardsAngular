05e02277227f2b47529453b717ccbd1a4ac43622
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-12 19:19:30 -0300

    shared

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components Pages
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+},
+{
+  path: 'account',
+  component: AccountComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
