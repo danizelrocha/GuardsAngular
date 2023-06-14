4e926853e2fd9effc66d6b81440d661a98c50cdc
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-06-14 09:01:45 -0300

    dashboard/ leads

--- rotas/src/app/core/core-routing.module.ts
+++ rotas/src/app/core/core-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LeadsComponent } from './pages/leads/leads.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: DashboardComponent
+  },
+  {
+    path:'leads',
+    component:LeadsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
